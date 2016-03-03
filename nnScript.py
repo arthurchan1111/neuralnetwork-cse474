4bd23ff36bfda917d44e11e07834f5a8fd32a6ee
Arthur T Chan <atchan2@timberlake.cse.buffalo.edu>
2016-03-03 18:39:43 -0500

    hi

--- nnScript.py
+++ nnScript.py
@@ -3,7 +3,7 @@ from scipy.optimize import minimize
 from scipy.io import loadmat
 from math import sqrt
 from math import exp
-
+np.set_printoptions(threshold=np.inf)
 
 def initializeWeights(n_in,n_out):
     """
@@ -69,22 +69,55 @@ def preprocess():
     allTrain = allTrain/255.0
     
     """Creates a 60000 x 10 label matrix corresponding to the allTrain matrix.  They are essentially in parallel so for example, the data at index 5 in allTrain will match up with the label at index 5 in allTrainLabel.  Each row is 10 units long and corresponds to a digit 0-9."""
-    allTrainLabel = np.concatenate(((np.full((len(mat.get('train0')), 10), [1,0,0,0,0,0,0,0,0,0])),(np.full((len(mat.get('train1')), 10), [0,1,0,0,0,0,0,0,0,0])),(np.full((len(mat.get('train2')), 10), [0,0,1,0,0,0,0,0,0,0])),(np.full((len(mat.get('train3')), 10), [0,0,0,1,0,0,0,0,0,0])),(np.full((len(mat.get('train4')), 10), [0,0,0,0,1,0,0,0,0,0])),(np.full((len(mat.get('train5')), 10), [0,0,0,0,0,1,0,0,0,0])),(np.full((len(mat.get('train6')), 10), [0,0,0,0,0,0,1,0,0,0])),(np.full((len(mat.get('train7')), 10), [0,0,0,0,0,0,0,1,0,0])),(np.full((len(mat.get('train8')), 10), [0,0,0,0,0,0,0,0,1,0])),(np.full((len(mat.get('train9')), 10), [0,0,0,0,0,0,0,0,0,1]))), axis=0)
+    allTrainLabel = np.concatenate(((np.full((len(mat.get('train0')), 1), 0)),(np.full((len(mat.get('train1')), 1), 1)),(np.full((len(mat.get('train2')), 1), 2)),(np.full((len(mat.get('train3')), 1), 3)),(np.full((len(mat.get('train4')), 1), 4)),(np.full((len(mat.get('train5')), 1), 5)),(np.full((len(mat.get('train6')), 1), 6)),(np.full((len(mat.get('train7')), 1), 7)),(np.full((len(mat.get('train8')), 1), 8)),(np.full((len(mat.get('train9')), 1), 9))), axis=0)
 
+    full=allTrain.shape[0]
+    split=allTrain.shape[0]*5/6
     """This splits and shuffles both the data and label matrixes in the same order so they will still match up into a validation set and a training set"""
-    seed = np.random.permutation(60000)
-    train_data = allTrain[seed[:50000],:]
-    validation_data = allTrain[seed[50000:],:]
-    train_label = allTrainLabel[seed[:50000],:]
-    validation_label = allTrainLabel[seed[50000:],:]
+    seed = np.random.permutation(full)
+    train_data = allTrain[seed[:split],:]
+    validation_data = allTrain[seed[split:],:]
+    train_label = allTrainLabel[seed[:split],:]
+    validation_label = allTrainLabel[seed[split:],:]
 
     """Stacks all test matrices into one 10000 x 784 matrix and normalizes it to the range [0,1]"""
     test_data = np.concatenate((mat.get('test0'),mat.get('test1'),mat.get('test2'),mat.get('test3'),mat.get('test4'),mat.get('test5'),mat.get('test6'),mat.get('test7'),mat.get('test8'),mat.get('test9')), axis=0)
     test_data = test_data/255.0
     
-    """Creates a 10000 x 10 label matrix corresponding to the test_data matrix.  They are essentially in parallel so for example, the data at index 5 in test_data will match up with the label at index 5 in test_label.  Each row is 10 units long and corresponds to a digit 0-9."""
-    test_label = np.concatenate(((np.full((len(mat.get('test0')), 10), [1,0,0,0,0,0,0,0,0,0])),(np.full((len(mat.get('test1')), 10), [0,1,0,0,0,0,0,0,0,0])),(np.full((len(mat.get('test2')), 10), [0,0,1,0,0,0,0,0,0,0])),(np.full((len(mat.get('test3')), 10), [0,0,0,1,0,0,0,0,0,0])),(np.full((len(mat.get('test4')), 10), [0,0,0,0,1,0,0,0,0,0])),(np.full((len(mat.get('test5')), 10), [0,0,0,0,0,1,0,0,0,0])),(np.full((len(mat.get('test6')), 10), [0,0,0,0,0,0,1,0,0,0])),(np.full((len(mat.get('test7')), 10), [0,0,0,0,0,0,0,1,0,0])),(np.full((len(mat.get('test8')), 10), [0,0,0,0,0,0,0,0,1,0])),(np.full((len(mat.get('test9')), 10), [0,0,0,0,0,0,0,0,0,1]))), axis=0)
+    find_dup= np.transpose(np.concatenate((train_data,validation_data,test_data),axis=0))
 
+    """This segment finds all elements of the data which never change so they can be removed"""
+    remove=np.ones((test_data.shape[1],1))
+    count=0
+    same=0
+    for x in find_dup:
+        oldp= x[0]
+        for p in x:
+            if p!=oldp:
+                remove[count][0]=0
+                same+=1
+                break
+            oldp=p
+        count+=1
+
+    r=np.zeros(test_data.shape[1]-same)
+    count=0
+    index=0
+    for x in remove:
+        if x==1:
+            r[index]=count
+            index+=1
+        count+=1
+    
+    
+
+    """Creates a 10000 x 10 label matrix corresponding to the test_data matrix.  They are essentially in parallel so for example, the data at index 5 in test_data will match up with the label at index 5 in test_label.  Each row is 10 units long and corresponds to a digit 0-9."""
+    test_label = np.concatenate(((np.full((len(mat.get('test0')), 1), 0)),(np.full((len(mat.get('test1')), 1), 1)),(np.full((len(mat.get('test2')), 1), 2)),(np.full((len(mat.get('test3')), 1), 3)),(np.full((len(mat.get('test4')), 1), 4)),(np.full((len(mat.get('test5')), 1), 5)),(np.full((len(mat.get('test6')), 1), 6)),(np.full((len(mat.get('test7')), 1), 7)),(np.full((len(mat.get('test8')), 1), 8)),(np.full((len(mat.get('test9')), 1), 9))), axis=0)
+
+    """columns that don't change between examples are removed"""
+    train_data=np.delete(train_data, r, axis=1)
+    validation_data=np.delete(validation_data, r, axis=1)
+    test_data=np.delete(test_data, r, axis=1)
     return train_data, train_label, validation_data, validation_label, test_data, test_label
     
     
@@ -127,40 +160,105 @@ def nnObjFunction(params, *args):
     % w2: matrix of weights of connections from hidden layer to output layers.
     %     w2(i, j) represents the weight of connection from unit j in hidden 
     %     layer to unit i in output layer."""
-    
+
     n_input, n_hidden, n_class, training_data, training_label, lambdaval = args
     
-    w1 = params[0:n_hidden * (n_input + 1)].reshape( (n_hidden, (n_input + 1)))
-    w2 = params[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
+    """translates label vector of digits 0-9 into 1-K form"""
+    count=0
+    label10=np.zeros((training_label.shape[0],10))
+    for x in training_label:
+        if(x==0):
+            label10[count]=[1,0,0,0,0,0,0,0,0,0]
+        elif(x==1):
+            label10[count]=[0,1,0,0,0,0,0,0,0,0]
+        elif(x==2):
+            label10[count]=[0,0,1,0,0,0,0,0,0,0]
+        elif(x==3):
+            label10[count]=[0,0,0,1,0,0,0,0,0,0]
+        elif(x==4):
+            label10[count]=[0,0,0,0,1,0,0,0,0,0]
+        elif(x==5):
+            label10[count]=[0,0,0,0,0,1,0,0,0,0]
+        elif(x==6):
+            label10[count]=[0,0,0,0,0,0,1,0,0,0]
+        elif(x==7):
+            label10[count]=[0,0,0,0,0,0,0,1,0,0]
+        elif(x==8):
+            label10[count]=[0,0,0,0,0,0,0,0,1,0]
+        else:
+            label10[count]=[0,0,0,0,0,0,0,0,0,1]
+        count+=1
+    
+    w1 = params[:n_hidden * (n_input+1)].reshape((n_hidden, (n_input+1)))
+    w2 = params[(n_hidden * (n_input+1)):].reshape((n_class, (n_hidden+1)))
     obj_val = 0  
     
+    print('in nnobj')
+
     #Get bias dimension
     bias_dimension = training_data.shape[0]
+
     #Fill it all with ones
-    bias = np.ones(bias_dimension,1)
+    bias = np.ones((bias_dimension,1))
+
     #Add bias to weights 
-    training_data_with_bias = np.concatenate((bias,training_data),1)
-    
+    training_data_with_bias = np.concatenate((training_data,bias),1)
+
     #Feed Foward Start By Multiplying Training data by weights of w1
-    z2 = training_data_with_bias * w1
+    z2 = np.dot(training_data_with_bias,np.transpose(w1))
+
     #Apply Sigmoid function
     a2= sigmoid(z2)
     #Apply Another Bias Dimension to the new matrix
-    a2_bias= np.concatenate((bias_dimension,a2),1)
-    
+
+    #bias_dimension = a2.shape[0]
+    bias = np.ones((bias_dimension,1))
+    a2_bias= np.concatenate((a2,bias),1)
+
     #Multiply new matrix by the weights of w2
-    z3= a2_bias * w2
-    
+    z3 = np.dot(a2_bias,np.transpose(w2))
+	
     #Apply Sigmoid Function to the new data
     y= sigmoid(z3)
+
+    #yl-ol (element of equation (9))
+    dif= label10-y
     
+    #1-ol (element of equation (9))
+    dif2= 1-y
+
     # Finish Forward Propagation
     
+    #equation (15)
+    obj_val = ((lambdaval/(2*y.shape[0]))*(np.sum(np.square(w1))+np.sum(np.square(w2))))+(np.sum(.5*np.sum(np.square(dif),axis=1))/y.shape[0])
+    
+    #column vector, equation (9)
+    elem1=np.transpose(np.array(-1*dif*dif2*y,ndmin=2))
+
+    #w2 matrix with bias cut out
+    w2trim= np.delete(w2,w2.shape[1]-1,1)
+
+    #equation (12) without multiplying the xi term yet
+    elem2=(-1*(1-a2)*(a2))*(np.dot((dif*dif2*y),w2trim))
+
+#summing up the inner part of equation (17)
+    total=np.zeros_like(w1)
+    for x in range(0,y.shape[0]):
+        total+=np.dot(np.transpose(np.array(elem2[x],ndmin=2)),np.array(training_data_with_bias[x],ndmin=2))
+
+    #equation (17)
+    grad_w1 = (total+(lambdaval*w1))/y.shape[0]
+
+    #equation (16)
+    grad_w2 = (np.dot(elem1,a2_bias)+(lambdaval*w2))/y.shape[0]
+
+    
+    
+    	
     #Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
     #you would use code similar to the one below to create a flat array
-    #obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
-    obj_grad = np.array([])
-    
+    obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
+    print (obj_val)
     return (obj_val,obj_grad)
 
 
@@ -185,7 +283,48 @@ def nnPredict(w1,w2,data):
     
     labels = np.array([])
     #Your code here
+    #Get bias dimension
+    bias_dimension = data.shape[0]
+
+    #Fill it all with ones
+    bias = np.ones((bias_dimension,1))
+
+    #Add bias to weights 
+    data_with_bias = np.concatenate((data,bias),1)
+
+    #Feed Foward Start By Multiplying Training data by weights of w1
+    z2 = np.dot(data_with_bias,np.transpose(w1))
+
+    #Apply Sigmoid function
+    a2= sigmoid(z2)
+    #Apply Another Bias Dimension to the new matrix
+
+    #bias_dimension=a2.shape[0]
+    #bias = np.ones((bias_dimension,1))
+    a2_bias= np.concatenate((a2,bias),1)
+
+    #Multiply new matrix by the weights of w2
+    z3 = np.dot(a2_bias,np.transpose(w2))
+	
+    #Apply Sigmoid Function to the new data
+    y= sigmoid(z3)
+
+    #find max value and add that digit to the labels vector
+    labels= np.zeros((y.shape[0],1))
+    count=0
+    for x in y:
+        index=0
+        max=0
+        inmax=0
+        for p in x:
+            if p >= max:
+                max=p
+                inmax=index
+            index+=1
+        labels[count][0]=inmax
+        count+=1
     
+    print('results n ', labels)
     return labels
     
 
@@ -195,17 +334,16 @@ def nnPredict(w1,w2,data):
 
 train_data, train_label, validation_data,validation_label, test_data, test_label = preprocess();
 
-
 #  Train Neural Network
 
 # set the number of nodes in input unit (not including bias unit)
 n_input = train_data.shape[1]; 
 
 # set the number of nodes in hidden unit (not including bias unit)
-n_hidden = 50;
-                   
+n_hidden = 16;
+				   
 # set the number of nodes in output unit
-n_class = 10;                   
+n_class = 10;				   
 
 # initialize the weights into some random matrices
 initial_w1 = initializeWeights(n_input, n_hidden);
@@ -215,8 +353,7 @@ initial_w2 = initializeWeights(n_hidden, n_class);
 initialWeights = np.concatenate((initial_w1.flatten(), initial_w2.flatten()),0)
 
 # set the regularization hyper-parameter
-lambdaval = 0;
-
+lambdaval = .5;
 
 args = (n_input, n_hidden, n_class, train_data, train_label, lambdaval)
 
@@ -255,4 +392,4 @@ predicted_label = nnPredict(w1,w2,test_data)
 
 #find the accuracy on Validation Dataset
 
-print('\n Test set Accuracy:' + + str(100*np.mean((predicted_label == test_label).astype(float))) + '%')
+print('\n Test set Accuracy:' + str(100*np.mean((predicted_label == test_label).astype(float))) + '%')
